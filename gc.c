andymeeus/ruby

commit 5878a2d3d087ab0a5b724dafe34014b5d8bbbbbc

* gc.c: needs malloc.h if malloc_usable_size() is available.
git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@43763 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

gc.c
@@ -39,6 +39,8 @@
 # ifdef _WIN32
 #   define malloc_usable_size(a) _msize(a)
 # endif
+#else
+# include <malloc.h>
 #endif
 
 #if /* is ASAN enabled? */ \

commit 11bb4f33dcd49851e0deec61939db2a50d57bd7d

* gc.c: fix build failure on FreeBSD introduced by r43763.   malloc_usable_size() is defined by malloc_np.h on FreeBSD.
* configure.in: check malloc.h and malloc_np.h.

git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@43769 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

configure.in
@@ -1091,6 +1091,8 @@ AC_CHECK_HEADERS( \
   process.h \
   sys/prctl.h \
   atomic.h \
+  malloc.h \
+  malloc_np.h \
   setjmpex.h
 )
 

gc.c
@@ -40,7 +40,11 @@
 #   define malloc_usable_size(a) _msize(a)
 # endif
 #else
-# include <malloc.h>
+# ifdef HAVE_MALLOC_H
+#  include <malloc.h>
+# elif defined(HAVE_MALLOC_NP_H)
+#  include <malloc_np.h>
+# endif
 #endif
 
 #if /* is ASAN enabled? */ \